4406e6520b1944cefa0a61449713bbe67949e59c
阿涵锅锅 <2893559070@qq.com>
2023-05-02 11:05:15 +0800

    docs-update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,7 @@ const sidebar = sideBar(path.resolve(__dirname, "../views"), '.md', 'docs');
 export default defineUserConfig({
   base: '/ahana-docs/',
   lang: 'zh-CN',
-  title: '啊涵的笔记（网上资料总结，如有侵权联系 qq: 2893559070 删除）',
+  title: '啊涵的笔记（网上资料总结，个人笔记，非商用，如有侵权联系 qq: 2893559070 删除）',
   description: '技术随记',
   theme: defaultTheme({
     navbar,
